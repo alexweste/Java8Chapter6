78fae73c541661490459d5c793bb4e10d16d571e
alexweste <alexandrzvarygin@gmail.com>
2018-03-12 23:33:29 +0300

    Ishodny klass Box

---
+++ src/Box.java
@@ -0,0 +1,12 @@
+// Ishodny klass Box
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    // vyvesti obyem parallelepipeda
+    void volume(){
+        System.out.print("Obyem raven ");
+        System.out.println(width * height * depth);
+    }
+}
