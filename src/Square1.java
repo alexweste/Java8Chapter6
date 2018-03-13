58aeff47f6252b69228a5213ff4c38dd8a78bd9d
alexweste <alexandrzvarygin@gmail.com>
2018-03-13 23:25:42 +0300

    Programma vychislyaet koren kvadratniy vo vneshnem metode square. Ishodnye dannye v metod square peredayutsya iz etoy programmy

--- src/Square1.java
+++ src/Square1.java
@@ -11,13 +11,13 @@ class Square1 {
     public static void main(String[] args) {
         int x, y;
         SquareI sq1 = new SquareI();
-        SquareI sq2 = new SquareI();
+
         x = sq1.square(4);
         System.out.println("x1= " + x);
         x = sq1.square(44);
         System.out.println("x2= " + x);
         y = 6;
-        x = sq2.square(y);
+        x = sq1.square(y);
         System.out.println("x3= " + x);
     }
 
